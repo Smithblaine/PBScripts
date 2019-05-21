750df6455cdcf5576f6ef53a951224e624cbe800
Blaine <Ringospy@gmail.com>
2019-05-21 17:37:04 -0500

    Creating powershell file that will copy entire folders for a user and put them on to a new computer

---
+++ Copy/copyfiles.ps1
@@ -0,0 +1,2 @@
+﻿$session = New-PSSession NKOVAR1
+Copy-Item -FromSession $session "C:\Users\Nathan.kovar" -Filter "*.*" -Exclude "*\Google\Chrome" -Recurse -Destination "C:\Users\blaine.smith\Desktop\Nathan" 
