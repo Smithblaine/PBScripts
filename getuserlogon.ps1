cf899bac4c1abfb232a6abafa00a94e925eace10
Blaine <Ringospy@gmail.com>
2019-06-17 21:19:38 -0500

    copy sign on logs

---
+++ getuserlogon.ps1
@@ -0,0 +1,5 @@
+﻿Get-EventLog security | 
+Where-Object {$_.TimeGenerated -gt '6/12/19'} | 
+Where-Object {($_.InstanceID -eq 4634) -or ($_.InstanceID -eq 4624)} | 
+Select-Object Index,machinename,TimeGenerated,InstanceID,Message | 
+Export-csv -LiteralPath C:\Users\Ringo\Desktop\test.csv
