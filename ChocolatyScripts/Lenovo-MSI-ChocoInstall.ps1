439d62a6c26e14e729664e06178bbbd7ef7e43bf
Blaine Smith <Ringospy@gmail.com>
2019-08-28 17:38:18 -0500

    updates

--- ChocolatyScripts/Lenovo-MSI-ChocoInstall.ps1
+++ ChocolatyScripts/Lenovo-MSI-ChocoInstall.ps1
@@ -7,18 +7,13 @@
 #     }
 #    }
 
-
-$TechnicalPrograms = 'firefox', 'googlechrome', 'unifying', 'adobereader', 'flashplayerplugin', 'jre8', 'javaruntime', 'zoom','notepadplusplus', 'intellijidea-ultimate', 'sql-server-management-studio', 'nodejs', 'postman', 'slack', 'git', 'mobaxterm', '7zip', 'office365proplus'
-
-$SimpleProgram = 'firefox', 'googlechrome', 'adobereader', 'flashplayerplugin', 'jre8', 'javaruntime', 'zoom','notepadplusplus', 'slack', '7zip', 'unifying', 'office365proplus'
+$homePrograms = 'unifying', 'git', 'mobaxterm', '7zip', 'vscode', 'eclipse', ''
 
 Write-Host "Which Installation would you like to install?
 
-1.) Technical Install?
-2.) Simple Installation?
-3.) Uninstall Technical Programs
-4.) Uninstall Simple Programs
-5.) Exit Program
+1.) Home Program Install?
+2.) Uninstall Home Programs
+3.) Exit Program
 ";
 
 $choice = Read-Host "Please enter your choice";
@@ -35,64 +30,28 @@ switch ($choice) {
     }
     
     If(Test-Path -Path "$env:ProgramData\Chocolatey") {
-        ForEach ($PackageName in $TechnicalPrograms) {
+        ForEach ($PackageName in $homePrograms) {
         choco install $PackageName -y
       }
     }
     Else {
         Set-ExecutionPolicy Bypass -Scope Process -Force; 
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        ForEach ($PackageName in $TechnicalPrograms) {
+        ForEach ($PackageName in $homePrograms) {
         choco install $PackageName -y
         }
     } 
         break;
     }
 
-    #Non-Techinal script
-    2 { 
-    Test-Path -Path "$env:ProgramData\Chocolatey"
-    
-    $ChocoInstalled = $false
-    if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
-        $ChocoInstalled = $true
-    }
-    else {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; 
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    }
-    
-    If(Test-Path -Path "$env:ProgramData\Chocolatey") {
-        ForEach ($PackageName in $SimpleProgram) {
-            choco install $PackageName -y
-        }
-    }
-    Else {
-        Set-ExecutionPolicy Bypass -Scope Process -Force; 
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        ForEach ($PackageName in $SimpleProgram) {
-            choco install $PackageName -y
-        }
-    } 
-        break;
-    }
-
-    #Uninstall Technical Programs
-    3 { If(Test-Path -Path "$env:ProgramData\Chocolatey") {
-        ForEach ($PackageName in $TechnicalPrograms) {
-            choco uninstall $PackageName -y
-        }
-    } 
-    }
-
-    #Uninstall Simple Programs
-    4 { If(Test-Path -Path "$env:ProgramData\Chocolatey") {
-        ForEach ($PackageName in $SimpleProgram) {
+    #Uninstall Home Programs
+    2 { If(Test-Path -Path "$env:ProgramData\Chocolatey") {
+        ForEach ($PackageName in $homePrograms) {
             choco uninstall $PackageName -y
         }
     } 
     }
 
     #Exiting the program if the user selects to.
-    5 { exit; }
+    3 { exit; }
 }
